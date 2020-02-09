f76b15dd43c32534b6afddf4731bc8355cff5856
Weliton Martins <weliton.martins@yaman.com>
2020-02-09 17:02:59 -0300

    desafio 2 aula 6

--- 05-desafio/desafio.js
+++ 05-desafio/desafio.js
@@ -1,4 +1,16 @@
+require('./extensao');
 const { categoriaFilmes } = require('./dados');
 
+const selecioneFilme = categoriaFilmes
+.map(categoria => categoria.videos)
+.concatAll()
+.map( filme => ({
+    id: filme.id,
+    title: filme.title,
+    boxart: filme.boxarts
+        .filter(b => b.width === 150 && b.height === 200)
+        .map(b => b.url)
+        .pop()//indexar
+})); 
 
-console.log(categoriaFilmes);
+console.log(selecioneFilme);
